94dd09003e525d3fe2a7d66ba446bcc72e8892e1
Jan Dragsbaek <wuptidoo@gmail.com>
2018-12-14 17:45:51 +0100

    Day 5, part 2

--- src/day5.ts
+++ src/day5.ts
@@ -1,35 +1,46 @@
 import * as fs from 'fs'
 import * as path from 'path'
 
-const data = fs.readFileSync(path.join(__dirname, './inputs/5'), 'utf-8')
+const polymer = fs.readFileSync(path.join(__dirname, './inputs/5'), 'utf-8')
 
-let final = data
+let reducedPolymer = fullyReduce(polymer)
+console.log(`Part 1: Final length: ${reducedPolymer.length}`)
 
-while(true){
-    let reduced = reduce(final)
-    console.log(`Reduced to: ${reduced}`)
-    if(final == reduced){
-        break
+let shortestPolymerLength = 99999999999999
+
+for(let i = 0; i < 26; i++){
+    let letterPair = `${String.fromCharCode(97+i)}${findOpposite(String.fromCharCode(97+i))}`
+
+    let partialPolymer = removeLetters(polymer, letterPair)
+    let reducedWithRemovedPair = fullyReduce(partialPolymer)
+    if(reducedWithRemovedPair.length < shortestPolymerLength){
+        shortestPolymerLength = reducedWithRemovedPair.length
+        console.log(`Found a shorter with letters ${letterPair}, that is ${shortestPolymerLength}`)
     }
-
-    final = reduced
 }
 
-console.log(`Part 1: Final length: ${final.length}`)
+console.log(`Part 2: Final length: ${shortestPolymerLength}`)
 
+function fullyReduce(polymer: string){
+    let final = polymer
+
+    while(true){
+        let reduced = reduce(final)
+        if(final == reduced){
+            return final
+        }
+    
+        final = reduced
+    }
+}
 
 function reduce(polymer: string){
     let res = []
     let units = polymer.split('')
     for(let i = 0; i <= units.length - 2; i++){
-        //console.log(units[i])
-        //console.log(`${units[i]} == ${findOpposite(units[i+1])}`)
-
         if(units[i] !== findOpposite(units[i+1])){
             res.push(units[i])
-            //console.log(`adding letter ${units[i]}`)
         } else {
-            //console.log('skipping')
             i++
         }
     }
@@ -43,4 +54,16 @@ function reduce(polymer: string){
 
 function findOpposite(c: string){
     return c == c.toLowerCase() ? c.toUpperCase() : c.toLowerCase()
+}
+
+function removeLetters(polymer: string, letters: string){
+    let final = []
+    for(let unit of polymer){
+        let found = false
+        for(let letter of letters){
+            if(unit == letter) { found = true }
+        }
+        if(!found) { final.push(unit) }
+    }
+    return final.join('')
 }
